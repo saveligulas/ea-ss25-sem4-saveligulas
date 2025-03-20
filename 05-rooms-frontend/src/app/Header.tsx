e1f8d586f95d3915750e650039e00556b09d7994
saveligulas <saveli.gulas@gmail.com>
2025-03-20 15:17:20 +0100

    .

---
+++ 05-rooms-frontend/src/app/Header.tsx
@@ -0,0 +1,38 @@
+import "../styles/header.css";
+
+async function Header() {
+    const response = await fetch(process.env.BASE_URL + "users/me")
+    const body = await response.json();
+
+    const firstName = body.firstName;
+    const lastName = body.lastName;
+    const avatar = body.portraitUrl;
+
+    return (
+        <nav className={"nav"}>
+            <ul className={"nav__list"}>
+                <li className={"nav__items"}>
+                    <a className={"nav__link"}>
+                        Rooms
+                    </a>
+                    <a className={"nav__link"}>
+                        Reservations
+                    </a>
+                </li>
+
+
+                <li className={"nav__user"}>
+                    <p>{firstName + " " + lastName}</p>
+                    <img
+                        className={"portrait__user"}
+                        src={avatar}
+                        alt={"avatar"}
+                    />
+                </li>
+            </ul>
+        </nav>
+
+    );
+}
+
+export default Header;
